a5895760c22864ac849bc4f5c74b85a0bbd37d8a
Ben Harris <bharris@colgate.edu>
2020-09-22 12:07:14 -0400

    Add plotting to init

--- pymn/__init__.py
+++ pymn/__init__.py
@@ -3,3 +3,4 @@ from .MetaNeighbor import MetaNeighbor
 from .MetaNeighborUS import MetaNeighborUS
 from .trainModel import trainModel
 from .variableGenes import variableGenes
+from .plotting import plotMetaNeighborUS, plotMetaNeighbor
