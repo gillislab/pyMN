68fa5526d5c247e48162b774c778b7245339c9e7
Ben Harris <bharris@cshl.edu>
2020-09-24 17:13:54 -0400

    Fix ncells

--- pymn/trainModel.py
+++ pymn/trainModel.py
@@ -15,12 +15,10 @@ def trainModel(adata, study_col, ct_col, var_genes='highly_variable'):
     dat = normalize_cells(adata.X).T
     labels = join_labels(adata.obs[study_col].values, adata.obs[ct_col].values)
     label_matrix = design_matrix(labels)
-    result = pd.DataFrame(dat @ label_matrix,
+    result = pd.DataFrame(dat @ label_matrix.values,
                           index=var_genes,
                           columns=label_matrix.columns)
-
-    n_cells = pd.Series(bottleneck.nansum(label_matrix, axis=0),
-                        index=label_matrix.columns,
-                        name='n_cells')
-
-    return pd.concat([result, n_cells])
+    n_cells = label_matrix.sum()
+    n_cells.name='n_cells'
+    n_cells = pd.DataFrame(n_cells)
+    return pd.concat([n_cells.T, result])
