a4bd05587031da3d731738c27e47676680ca43cd
Ben Harris <bharris@colgate.edu>
2020-09-25 12:11:54 -0400

    refactor split train/test

--- pymn/splitClusters.py
+++ pymn/splitClusters.py
@@ -25,8 +25,13 @@ def splitClusters(data, k, mn_key='MetaNeighborUS', save_uns =True):
 		return res
 
 
-##TODO: Refactor these two to save in uns
-def splitTrainClusters(mn_scores,k):
+
+def splitTrainClusters(data,k, mn_key='MetaNeighborUS',save_uns=True):
+	if type(data) is AnnData:
+		assert mn_key in data.uns_keys(), f'{mn_key} not found in uns, run MetaNeighborUS or pass mn_data'
+		mn_scores = data.uns[mn_key]
+	else:
+		mn_scores = data
 	row_is_na = np.all(np.isnan(mn_scores), axis=1)
 	col_is_na = np.all(np.isnan(mn_scores), axis=0)
 
@@ -34,7 +39,22 @@ def splitTrainClusters(mn_scores,k):
 	linkage = hierarchy.linkage(distance.pdist(mn_s.values).T,method='average')
 	membership = hierarchy.cut_tree(linkage, n_clusters=k)
 	membership_series = pd.Series(np.ravel(membership), index=mn_scores.index[is_na])
-	return [membership_series.index[membership_series == i].values for i in range(k)]
+	
+	res = [membership_series.index[membership_series == i].values for i in range(k)]
+	if save_uns and type(data) is AnnData:
+		data.uns[f'{mn_key}_split_train_{k}'] = res
+	else:
+		return res
 
-def splitTestClusters(mn_scores, k):
-	return splitTrainClusters(mn_scores.T, k)
+def splitTestClusters(data,k, mn_key='MetaNeighborUS',save_uns=True):
+	if type(data) is AnnData:
+		assert mn_key in data.uns_keys(), f'{mn_key} not found in uns, run MetaNeighborUS or pass mn_data'
+		mn_scores = data.uns[mn_key]
+	else:
+		mn_scores = data
+	
+	res  = splitTrainClusters(mn_scores, k, save_uns=False)
+	if save_uns and type(data) is AnnData:
+		data.uns[f'{mn_key}_split_test_{k}'] = res
+	else:
+		return res
